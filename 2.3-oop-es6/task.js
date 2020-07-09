f197371341463d954d4e7093a63101a9c835cebc
Irina Paukova <i.paukova@mail.ru>
2020-07-09 08:22:12 +0300

    первое и второе задание без третьего

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -49,43 +49,75 @@ class Book extends PrintEditionItem {
 
 class NovelBook  extends Book {
 	constructor(author, name, releaseDate, pagesCount) {
-		super(name, releaseDate, pagesCount)
+		super(author, name, releaseDate, pagesCount)
 		this.type = 'novel';
-		this.author = author;
 	}
 }
 
 class FantasticBook  extends Book {
 	constructor(author, name, releaseDate, pagesCount) {
-		super(name, releaseDate, pagesCount)
+		super(author, name, releaseDate, pagesCount)
 		this.type = 'fantastic';
-		this.author = author;
 	}
 }
 
 class DetectiveBook  extends Book {
 	constructor(author, name, releaseDate, pagesCount) {
-		super(name, releaseDate, pagesCount)
+		super(author, name, releaseDate, pagesCount)
 		this.type = 'detective';
-		this.author = author;
 	}
 }
 
 
 const picknick = new FantasticBook("Аркадий и Борис Стругацкие", "Пикник на обочине", 1972, 168);
 
-console.log(picknick.author); //"Аркадий и Борис Стругацкие"
+console.log(picknick.author); 
 picknick.state = 10;
-console.log(picknick.state); //10
+console.log(picknick.state); 
 picknick.fix();
-console.log(picknick.state); //15
+console.log(picknick.state); 
 
 //Задача 2
 
 class Library {
 	constructor(name) {
 		this.name = name;
-		this.book = [];	
+		this.books = [];	
+	};
+	addBook(book) {
+		if (book.state > 30) {
+			this.books.push(book);
+		} 
+	};
+	findBookBy(types, value) {
+		for (var i = 0; i < this.books.length; i++) {
+			if (this.books[i][types] === value) {
+				return this.books[i]; 
+			} 
+		}
+		return null;
+	};
+	giveBookByName(bookName) {
+		for (var i = 0; i < this.books.length; i++) {
+			if (this.books[i].name === bookName) {
+				return this.books.splice(i, 1); // нужно возвращать удаленную книгу, но метод splice возвращает массив с оставшимися элементами
+			} 
+		}
+		return null;
 	}
-	addBook(book){this.scores.push(rating)}
 };
+
+const library = new Library("Библиотека имени Ленина");
+const sherlokBook = new DetectiveBook("Артур", "о Шерлоке Холмсе", 2019, 1008);
+const sherlokBook2 = new DetectiveBook("Артур", "о Шерлоке Холмсе 2", 2020, 1008)
+
+library.addBook(sherlokBook);
+library.addBook(sherlokBook2);
+console.log(sherlokBook.author); 
+sherlokBook.state = 10;
+console.log(sherlokBook.state); 
+sherlokBook.fix();
+console.log(sherlokBook.state); 
+console.log(library.findBookBy("releaseDate", 2020));
+console.log(library.giveBookByName("о Шерлоке Холмсе 2"));
+console.log(library.books); 
