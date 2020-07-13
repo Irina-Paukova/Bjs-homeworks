8c87a409b4724b65a3ac77baf9f4934dddcc373b
Irina Paukova <i.paukova@mail.ru>
2020-07-13 14:25:42 +0300

    1.0

--- 3.2-array-proto-hocs/task.js
+++ 3.2-array-proto-hocs/task.js
@@ -1,17 +1,58 @@
-function logArray(arr) {
-	if(!Array.isArray(arr)) {
-		throw newError('Аргумент не является массивом');// выкидываем исключение
-	}
-	console.log(`Массив размером ${arr.length}: [`);
-	for(let item of arr) {
-		console.log(`${item} (${typeof item})`);
-	}
-	console.log(`]`);
+// function logArray(arr) {
+// 	if(!Array.isArray(arr)) {
+// 		throw newError('Аргумент не является массивом');// выкидываем исключение
+// 	}
+// 	console.log(`Массив размером ${arr.length}: [`);
+// 	for(let item of arr) {
+// 		console.log(`${item} (${typeof item})`);
+// 	}
+// 	console.log(`]`);
+// }
+
+// try {
+// 	logArray([12, 5]);
+// } catch(e) { // отлавливаем исключение
+// 	console.log(e);
+// }
+// // Error: Аргумент не является массивом
+
+// Задание 1
+
+function sleep(milliseconds) {
+  let e = new Date().getTime() + milliseconds;
+  while (new Date().getTime() <= e) {}
 }
 
-try {
-	logArray([12, 5]);
-} catch(e) { // отлавливаем исключение
-	console.log(e);
+function sum(...args) {
+  // Замедление на половину секунды.
+  sleep(100); 
+  return args.reduce((sum, arg) => {
+    return sum += +arg;
+  }, 0);
 }
-// Error: Аргумент не является массивом
+
+function compareArrays(arr1, arr2) {
+	return arr1.length == arr2.length && arr1.every((a,i) => a === arr2[i]);
+}
+
+function memorize(fn, limit) {
+    let memory = [];
+     
+    return function inner(...args) {
+        let findResult = memory.find(elem => compareArrays(elem.args, args));
+        let result = findResult ? findResult.result : fn(...args);
+
+        if (!findResult) {
+            memory.push({args: args, result: result});        
+        }
+
+        if (memory.length > limit) {
+            memory.shift();
+        }
+        return result;
+    }
+}
+
+const mSum = memorize(sum, 5);
+console.log(mSum(5, 4));
+console.log(mSum(5, 4));
